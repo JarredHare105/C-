87c3c370270604291b3ce1690ef18741ab6db853
Jarred Hare <84694816+JarredHare105@users.noreply.github.com>
2022-01-19 21:31:21 -0600

    Add files via upload

---
+++ Division W Remainder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DivisionWRemainder
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many eggs are there? ");
+            String eggVal = Console.ReadLine();
+            int eggs = Convert.ToInt32(eggVal);
+            Console.WriteLine("How many people are eating? ");
+            String peopleVal = Console.ReadLine();
+            int people = Convert.ToInt32(peopleVal);
+            int eggsSplit = eggs / people;
+            int remainder = eggs % people;
+            Console.WriteLine("Each person gets " + eggsSplit + " egg(s) " + "there is " + remainder + " egg(s) leftover");
+
+        }
+    }
+}
