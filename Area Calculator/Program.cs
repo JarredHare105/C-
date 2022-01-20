a3d88103b3097ad593b28edd18ffc7f2d205755b
Jarred Hare <84694816+JarredHare105@users.noreply.github.com>
2022-01-19 21:17:51 -0600

    Add files via upload

---
+++ Area Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AreaCalculator
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the Base? ");
+            String baseVal = Console.ReadLine();
+            double Base = Convert.ToDouble(baseVal);
+            Console.WriteLine("What is the Height? ");
+            String heightVal = Console.ReadLine();
+            double Height = Convert.ToDouble(heightVal);
+            double Area = (Base * Height)/2;
+            Console.WriteLine("Area = " + Area);
+        }
+    }
+}
